a569434c0f8462d8f12b64fdd6a351b942dc0f66
DC23 <DC23@users.noreply.github.com>
2026-06-07 18:31:06 +1000

    remove docked panel code from settings refs #19

--- src/settings.mjs
+++ src/settings.mjs
@@ -10,7 +10,6 @@ export const SETTINGS = {
     UI_BUTTON_HOVERED_COLOR: 'uiButtonHoveredColour',
     UI_BUTTON_CLICKED_COLOR: 'uiButtonClickedColour',
     UI_BACKGROUND_COLOR: 'uiBackgroundColour',
-    // FLOATING_UI_PANEL: 'uiInFrame',
     FLOATING_UI_PANEL_POSITION: 'uiPanelPosition',
     CURRENT_CHAOS_FACTOR: 'currentChaosFactor',
 }
@@ -25,17 +24,6 @@ export function registerSettings () {
     // Register the menus
     // I have no settings menus at the moment
     // registerSettingsMenu()
-
-    // docked panel is not supported, so hide the setting
-    // game.settings.register(MODULE_ID, SETTINGS.FLOATING_UI_PANEL, {
-    //     name: 'GTSV.Settings.ShowUIInFloatingWindow.name',
-    //     hint: 'GTSV.Settings.ShowUIInFloatingWindow.hint',
-    //     scope: 'client',
-    //     config: true,
-    //     type: Boolean,
-    //     default: false,
-    //     requiresReload: true,
-    // })
 
     game.settings.register(MODULE_ID, SETTINGS.CURRENT_CHAOS_FACTOR, {
         scope: 'client',
