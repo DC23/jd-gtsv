8a245f17ab93f97f1c0f649e63fa8ce93737231f
DC23 <DC23@users.noreply.github.com>
2026-06-07 10:47:25 +1000

    localised constants table for chaos factors

--- src/constants.mjs
+++ src/constants.mjs
@@ -8,4 +8,13 @@ import { MODULE_ID, SETTINGS } from './settings.mjs'
  * @public
  */
 export class Constants {
+    static CHAOS_FACTORS = [
+        { key: "GTSV.Chaos.Boring", value: "d20"},
+        { key: "GTSV.Chaos.UnderControl", value: "d12"},
+        { key: "GTSV.Chaos.Average", value: "d10"},
+        { key: "GTSV.Chaos.OutOfControl", value: "d8"},
+        { key: "GTSV.Chaos.Madness", value: "d6"},
+        { key: "GTSV.Chaos.AbjectChaos", value: "d5"},
+        { key: "GTSV.Chaos.PlaythingOfTheGods", value: "d4"},
+    ]
 }
